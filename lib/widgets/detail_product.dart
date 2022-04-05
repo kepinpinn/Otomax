a66099afa46300eb90fb22bffc7767f85e448f22
kelvin lianto <kelvinlianto@gmail.com>
2022-04-05 18:20:12 +0700

    Detail etalase produk

--- lib/widgets/detail_product.dart
+++ lib/widgets/detail_product.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:otomax/theme.dart';
 import 'package:otomax/widgets/category.dart';
 import 'package:otomax/widgets/collections.dart';
+import 'package:otomax/widgets/detail_screen.dart';
 import 'dart:async';
 import 'dart:convert';
 
@@ -10,8 +13,65 @@ class DetailProduct extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(text)),
-      body: SafeArea(child: ListView()),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(text,
+        style: blackTextStyle.copyWith(fontSize: 25)
+       ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: GridView.builder(
+                itemCount: 8, //banyaknya produk kategori
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, 
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  childAspectRatio: 0.75), 
+                itemBuilder: (context, index) => DetailCard(),
+                ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class DetailCard extends StatelessWidget {
+  const DetailCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.circular(16)
+          ),
+        child: Image.asset('assets/promo.png'),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0),
+          child: Text(
+            'Enkei Ring 16', 
+            style: blackTextStyle.copyWith(fontSize: 20)
+            ),
+        ),
+        Text(
+          "Rp 1.500.000", 
+          style: TextStyle(fontWeight: FontWeight.bold)
+          ),
+      ],
     );
   }
 }
